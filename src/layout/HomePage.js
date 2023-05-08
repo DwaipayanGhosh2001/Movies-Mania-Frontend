607817a4c5da3e1165690dd30959346732d213f8
Dwaipayan Ghosh <dwaipayanghosh3@gmail.com>
2023-05-08 14:29:46 +0530

    search added

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FiSearch } from "react-icons/fi";
 import {
   Container,
   Button,
@@ -8,20 +9,35 @@ import {
   CardTitle,
   Card,
   CardFooter,
+  Input,
+  InputGroup,
+  Form,
 } from "reactstrap";
 import TypingEffect from "../component/TypingEffect";
 import { Link } from "react-router-dom";
 import { useChannelRecord } from "../context/context";
-import { motion } from "framer-motion"; 
+import { motion } from "framer-motion";
 import axios from "axios";
 
 const HomePage = () => {
+  const searchRef = useRef(null);
   const { setChannelDetails } = useChannelRecord();
   const [channel, setChannel] = useState([]);
+  const [searchvalue, setSearchValue] = useState("");
 
   const handleSubmit = (name) => {
     setChannelDetails(name);
   };
+
+  //   const handleSearch = (e) => {
+  // e.preventDefault();
+  // setSearchValue(searchRef.current.value);
+  //   }
+
+  const searchLoad = channel.filter((item) =>
+    item.name.toLowerCase().startsWith(searchvalue.toLowerCase())
+  );
+  console.log(searchLoad);
   const fetchEvent = () => {
     axios
       .get("http://127.0.0.1:8000")
@@ -44,7 +60,8 @@ const HomePage = () => {
       >
         <TypingEffect text="Movie Mania" />
       </h1>
-        <motion.div className="ms-5"
+      <motion.div
+        className="ms-5"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
@@ -52,54 +69,125 @@ const HomePage = () => {
           delay: 1.5,
           ease: [0, 0.71, 0.2, 1.01],
         }}
+      >
+        <h3
+          className=" text-white ms-5 text-uppercase mb-3"
+          style={{ fontFamily: "monospace", letterSpacing: "5px" }}
         >
-          <h3
-            className=" text-white ms-5 text-uppercase mb-3"
-            style={{ fontFamily: "monospace", letterSpacing: "5px" }}
-          >
-            {" "}
-            Channels
-          </h3>
-        
-        <Container fluid>
-          <Row className="ms-5">
-            {channel.map((item, index) => (
-              <Col lg={3} md={4} sm={7} xs={8} key={index} className="m-5 ">
-                <Card className=" bg-background  d-flex flex-column h-100 shadow rounded">
-                  <img
-                    src={item.image}
-                    alt=""
-                    className="rounded border border-dark"
-                    style={{ width: "100%", height: "200px" }}
-                  />
-                  <CardTitle
-                    className="text-center  text-white fw-bold fs-4 my-3"
-                    style={{ fontFamily: "serif" }}
-                  >
-                    {item.name}
-                  </CardTitle>
-                  <CardBody className="flex-grow-1">
-                    <p className="text-center mb-0  text-white">
-                      <i>{item.description}</i>
-                    </p>
-                  </CardBody>
-                  <CardFooter className="border-0 mx-auto mb-3 mt-0">
-                    <Button
-                      className=" rounded text-white px-5 bg-button text-uppercase zoom"
-                      tag={Link}
-                      to="movielist"
-                      outline
-                      onClick={() => handleSubmit(item.name)}
+          {" "}
+          Channels
+          <span className="float-end me-5">
+            <Form>
+              <Input
+                type="search"
+                placeholder="Looking for ?"
+                className="border-0 rounded-pill px-3"
+                value={searchvalue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            </Form>
+          </span>
+        </h3>
+        {!searchvalue ? (
+          <Container fluid>
+            <Row className="ms-5">
+              {channel.map((item, index) => (
+                <Col lg={3} md={4} sm={7} xs={8} key={index} className="m-5 ">
+                  <Card className=" bg-background  d-flex flex-column h-100 shadow rounded">
+                    <img
+                      src={item.image}
+                      alt=""
+                      className="rounded border border-dark"
+                      style={{ width: "100%", height: "200px" }}
+                    />
+                    <CardTitle
+                      className="text-center  text-white fw-bold fs-4 my-3"
+                      style={{ fontFamily: "serif" }}
                     >
-                      Movie list
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-        </motion.div>
+                      {item.name}
+                    </CardTitle>
+                    <CardBody className="flex-grow-1">
+                      <p className="text-center mb-0  text-white">
+                        <i>{item.description}</i>
+                      </p>
+                    </CardBody>
+                    <CardFooter className="border-0 mx-auto mb-3 mt-0">
+                      <Button
+                        className=" rounded text-white px-5 bg-button text-uppercase zoom"
+                        tag={Link}
+                        to="movielist"
+                        outline
+                        onClick={() => handleSubmit(item.name)}
+                      >
+                        Movie list
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        ) : (
+          <>
+            <Container fluid>
+              <Row className="ms-5">
+                {searchLoad.length === 0 ? (
+                  <h2 className="text-white text-center mt-5 text-uppercase">
+                    {" "}
+                    No result!
+                  </h2>
+                ) : (
+                  <>
+                    <Row>
+                      {searchLoad.map((item, index) => (
+                        <Col
+                          lg={3}
+                          md={4}
+                          sm={7}
+                          xs={8}
+                          key={index}
+                          className="m-5 "
+                        >
+                          <Card className=" bg-background  d-flex flex-column h-100 shadow rounded">
+                            <img
+                              src={item.image}
+                              alt=""
+                              className="rounded border border-dark"
+                              style={{ width: "100%", height: "200px" }}
+                            />
+                            <CardTitle
+                              className="text-center  text-white fw-bold fs-4 my-3"
+                              style={{ fontFamily: "serif" }}
+                            >
+                              {item.name}
+                            </CardTitle>
+                            <CardBody className="flex-grow-1">
+                              <p className="text-center mb-0  text-white">
+                                <i>{item.description}</i>
+                              </p>
+                            </CardBody>
+                            <CardFooter className="border-0 mx-auto mb-3 mt-0">
+                              <Button
+                                className=" rounded text-white px-5 bg-button text-uppercase zoom"
+                                tag={Link}
+                                to="movielist"
+                                outline
+                                onClick={() => handleSubmit(item.name)}
+                              >
+                                Movie list
+                              </Button>
+                            </CardFooter>
+                          </Card>
+                        </Col>
+                      ))}
+                    </Row>
+                  </>
+                )}
+              </Row>
+            </Container>
+          </>
+        )}
+      </motion.div>
     </div>
   );
 };
--- src/layout/Movies.js
+++ src/layout/Movies.js
@@ -1,60 +1,143 @@
 import React, { useEffect, useState } from "react";
-import { Container, Button, Card, CardBody, CardTitle, Row, Col } from "reactstrap";
-import {FaArrowCircleLeft} from "react-icons/fa"
+import {
+  Container,
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+  Input,
+  Form,
+} from "reactstrap";
+import { FaArrowCircleLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { hover } from "@testing-library/user-event/dist/hover";
 import { useChannelRecord } from "../context/context";
 import axios from "axios";
-const Movies = () =>
-{
-  const {channelDetails} = useChannelRecord();
-  const[movies, setMovies] = useState([])
-  const fetchMovies = () =>{
-    axios.get(`http://127.0.0.1:8000/channels/${channelDetails}/`)
-    .then((res)=> {
-const {data} = res;
-setMovies(data.movies);
-    })
-  }
-  useEffect(()=>{
-   fetchMovies();
-  },[])
-return(
+const Movies = () => {
+  const [value, setValue] = useState("");
+  const { channelDetails } = useChannelRecord();
+  const [movies, setMovies] = useState([]);
+
+  const loadSearch = movies.filter((item) =>
+  item.name.toLowerCase().startsWith(value.toLowerCase())
+);
+
+  const fetchMovies = () => {
+    axios
+      .get(`http://127.0.0.1:8000/channels/${channelDetails}/`)
+      .then((res) => {
+        const { data } = res;
+        setMovies(data.movies);
+      });
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
     <Container fluid>
-        <div className="">
-        <Button tag={Link} to="/" className="border-0 float-end " outline style={{backgroundColor:hover="transparent"}}>
-                 <FaArrowCircleLeft className="fs-1 text-danger " />
-            </Button>
-            <h1 className="text-white text-center pt-5 text-uppercase mt-3 ms-5" style={{letterSpacing: "10px"}}>{channelDetails} </h1>   
-        </div>
-        <Container fluid>
-        <Row className="ms-5">
-          {movies.map((item, index) => (
-            <Col lg={3} md={4} sm={7} xs={8} key={index} className="m-5 ">
-              <Card className=" bg-background  d-flex flex-column h-100 shadow rounded">
-                <img
-                  src={`http://127.0.0.1:8000/${item.image}`}
-                  alt=""
-                  className="rounded border border-dark"
-                  style={{width: "100%", height: "300px"}}
-                />
-                <CardTitle
-                  className="text-center  text-white fw-bold fs-4 my-3"
-                  style={{ fontFamily: "serif" }}
-                >
-                  {item.name}
-                </CardTitle>
-                <CardBody className="flex-grow-1">
-                  <p className="text-center mb-0  text-white">
-                    <i>Duration : {item.duration}</i>
-                  </p>
-                </CardBody>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-        </Container>
+      <div className="">
+      <Form className="float-start mt-2">
+        <Input
+          type="search"
+          placeholder="Looking for?"
+          className="w-100 ms-5 border-0 rounded-pill"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </Form>
+        <Button
+          tag={Link}
+          to="/"
+          className="border-0 float-end "
+          outline
+          style={{ backgroundColor: (hover = "transparent") }}
+        >
+          <FaArrowCircleLeft className="fs-1 text-danger " />
+        </Button>
+        <h1
+          className="text-white text-center pt-5 text-uppercase mt-3 ms-5"
+          style={{ letterSpacing: "10px" }}
+        >
+          {channelDetails}{" "}
+        </h1>
+      </div>
+     
+
+      <div>
+        {!value ? (
+          <Container fluid>
+            <Row className="ms-5">
+              {movies.map((item, index) => (
+                <Col lg={3} md={4} sm={7} xs={8} key={index} className="m-5 ">
+                  <Card className=" bg-background  d-flex flex-column h-100 shadow rounded">
+                    <img
+                      src={`http://127.0.0.1:8000/${item.image}`}
+                      alt=""
+                      className="rounded border border-dark"
+                      style={{ width: "100%", height: "300px" }}
+                    />
+                    <CardTitle
+                      className="text-center  text-white fw-bold fs-4 my-3"
+                      style={{ fontFamily: "serif" }}
+                    >
+                      {item.name}
+                    </CardTitle>
+                    <CardBody className="flex-grow-1">
+                      <p className="text-center mb-0  text-white">
+                        <i>Duration : {item.duration}</i>
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        ) : (
+          <>
+            <div>
+              {loadSearch === 0 ? (
+                <h2 className="text-white text-center mt-5 text-uppercase">
+                  {" "}
+                  No result!
+                </h2>
+              ) : (
+                <>
+                 <Container fluid>
+            <Row className="ms-5">
+              {loadSearch.map((item, index) => (
+                <Col lg={3} md={4} sm={7} xs={8} key={index} className="m-5 ">
+                  <Card className=" bg-background  d-flex flex-column h-100 shadow rounded">
+                    <img
+                      src={`http://127.0.0.1:8000/${item.image}`}
+                      alt=""
+                      className="rounded border border-dark"
+                      style={{ width: "100%", height: "300px" }}
+                    />
+                    <CardTitle
+                      className="text-center  text-white fw-bold fs-4 my-3"
+                      style={{ fontFamily: "serif" }}
+                    >
+                      {item.name}
+                    </CardTitle>
+                    <CardBody className="flex-grow-1">
+                      <p className="text-center mb-0  text-white">
+                        <i>Duration : {item.duration}</i>
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+                </>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </Container>
-)
-}
+  );
+};
 export default Movies;
